c9ad6824a2d73462d612d7f25211f0ea52ad0d61
iamemmykeys <afolabiemmanuelayo90@gmail.com>
2023-02-23 08:35:47 -0800

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * mian - test function that prints if integer is positive or negative
+ * mian - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
