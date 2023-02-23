7702279e1129fc11ca965fc7e9eee081f75f7b0a
iamemmykeys <afolabiemmanuelayo90@gmail.com>
2023-02-23 01:20:40 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@
 
 void print_to_98(int n)
 
-{       
+{
 	
 	if (n <= 98)
 	{       
@@ -31,7 +31,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (; n>= 98; n--)
+	for (; n >= 98; n--)
 	{
 	if (n == 98)
 	{
