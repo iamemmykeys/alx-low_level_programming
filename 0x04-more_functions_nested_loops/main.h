0f30778cd14085dd1d0296e8c45f6295864b0ce0
iamemmykeys <afolabiemmanuelayo90@gmail.com>
2023-02-26 11:16:00 -0800

    2. Collaboration is multiplication

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,9 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
+int mul(int x, int y);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
