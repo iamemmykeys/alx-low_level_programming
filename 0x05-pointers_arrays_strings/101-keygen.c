7227b542b1b2ad7fd6e1d190afb913a1202d3bb0
iamemmykeys <afolabiemmanuelayo90@gmail.com>
2023-02-28 11:24:51 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,7 +9,6 @@
  */
 
 int main(void)
-
 {
 	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
@@ -17,7 +16,6 @@ int main(void)
 	srand(time(0));
 
 	while (sum < 2772)
-
 	{
 	password[index] = 33 + rand() % 94;
 	sum += password[index++];
