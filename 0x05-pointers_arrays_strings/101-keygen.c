c6c1623c0830041b631337ee10cc6ed79e91d85c
iamemmykeys <afolabiemmanuelayo90@gmail.com>
2023-02-28 11:15:16 -0800

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,19 +1,53 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * _strcpy - Copy a string
- * @dest: Destination value
- * @src: Source value
- * Return: the pointer to dest
+ * main - Generates random valid passwords for the
+ * program 101-crackme.
+ * Return: Always 0.
  */
 
-char *_strcpy(char *dest, char *src)
+int main(void)
+
 {
-int i;
-for (i = 0; src[i] != '\0'; i++)
-{
-dest[i] = src[i];
-}
-dest[i++] = '\0';
-return (dest);
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+
+	{
+	password[index] = 33 + rand() % 94;
+	sum += password[index++];
+	}
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+	diff_half1 = (sum - 2772) / 2;
+	diff_half2 = (sum - 2772) / 2;
+
+	if ((sum - 2772) % 2 != 0)
+	diff_half1++;
+
+	for (index = 0; password[index]; index++)
+	{
+
+	if (password[index] >= (33 + diff_half2))
+	{
+	password[index] -= diff_half1;
+	break;
+	}
+	}
+	for (index = 0; password[index]; index++)
+	{
+	if (password[index] >= (33 + diff_half2))
+	{
+	password[index] -= diff_half2;
+	break;
+	}
+	}
+	}
 }
