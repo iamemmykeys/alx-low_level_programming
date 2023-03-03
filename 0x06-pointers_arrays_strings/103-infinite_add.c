783164d9fa2dd5be99f9d24e3d3871ff18d425f8
iamemmykeys <afolabiemmanuelayo90@gmail.com>
2023-03-03 14:15:25 -0800

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
+ * rev_string - Reverses array
  * @n: integer params
  * Return: 0
  */
 
 void rev_string(char *n)
 {
-        int i = 0;
-        int j = 0;
-        char temp;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-        while (*(n + i) != '\0')
-        {
-                i++;
+	while (*(n + i) != '\0')
+	{
+	i++;
 	}
 }
 
@@ -29,30 +29,30 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {       
-        int overflow = 0, i = 0, j = 0, digits = 0;
-        int val1 = 0, val2 = 0, temp_tot = 0;
-
-        while (*(n1 + i) != '\0')
-        i++;
-        while (*(n2 + j) != '\0')
-        j++;
-        i--;
-        j--;
-        if (j >= size_r || i >= size_r)
-        return (0);
-        while (j >= 0 || i >= 0 || overflow == 1)
-        {
-        if (i < 0)
-        val1 = 0;
-        else
-        val1 = *(n1 + i) - '0';
-        if (j < 0)
-        val2 = 0;
-        else
-        val2 = *(n2 + j) - '0';
-        temp_tot = val1 + val2 + overflow;
-        if (temp_tot >= 10)
-	overflow = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
+	
+	while (*(n1 + i) != '\0')
+	i++;
+	while (*(n2 + j) != '\0')
+	j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+	return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+	if (i < 0)
+	val1 = 0;
+	else
+	val1 = *(n1 + i) - '0';
+	if (j < 0)
+	val2 = 0;
+	else
+	val2 = *(n2 + j) - '0';
+	temp_tot = val1 + val2 + overflow;
+	if (temp_tot >= 10)
+	overflow = 1;
 	else
 	overflow = 0;
 	if (digits >= (size_r -1))
